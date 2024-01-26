a5717fc2e964cea6f9c3f6c58cd8a583b3d6d350
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-26 17:18:50 +0600

    variable-types added

---
+++ types.js
@@ -0,0 +1,16 @@
+// string
+var name = "Omor Faruk";
+var address = "Dhaka-1000";
+var country = 'Bangladesh';
+
+// boolean
+var isRich = true;
+var passed = false;
+
+// number
+var price = 5.5;
+
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+console.log(typeof price);
