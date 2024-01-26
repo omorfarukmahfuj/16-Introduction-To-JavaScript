780bd1315c5454a480db28675b99f24f0b1648e8
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-26 21:09:24 +0600

    task2 added

---
+++ Practice Task/task2.js
@@ -0,0 +1,7 @@
+var mathematicsMark = 75.25;
+var biologyMark = 65;
+var chemistryMark = 80;
+var physicsMark = 35.45;
+var banglaMark = 99.50;
+var averageMark = (mathematicsMark+biologyMark+chemistryMark+physicsMark+banglaMark)/5;
+console.log(averageMark.toFixed(2));
