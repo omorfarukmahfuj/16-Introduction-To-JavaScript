118704ef126d4a98da97b312451a44965791bc16
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-26 19:09:13 +0600

    artihmetic-operations added

---
+++ operators.js
@@ -0,0 +1,10 @@
+// Arithmetic Operator
+
+var onionPrice = 20;
+var eggPrice = 10;
+
+console.log(onionPrice+eggPrice);
+console.log(onionPrice-eggPrice);
+console.log(onionPrice*eggPrice);
+console.log(onionPrice/eggPrice);  
+console.log(onionPrice%eggPrice); 
